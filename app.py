856bf666002cebd3e18482d91a41fdb45902d21d
Masterhazi <86478294+Masterhazi@users.noreply.github.com>
2024-10-23 08:34:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -225,11 +225,8 @@ if st.button("Search") and query:
                 scholar_article = scholarly.fill(scholar_article)  # Fetch full details
                 ris_file = create_ris_file(scholar_article)
                 st.text_area("RIS File", ris_file, height=300)
-                if len(scholar_article['bib'].get('title'))>1:
-                    title = " ".join(scholar_article['bib'].get('title'))
-                else:
-                    title = scholar_article['bib'].get('title')
-                st.download_button("Download RIS", ris_file, file_name=f"{title}.ris")
+
+                st.download_button("Download RIS", ris_file, file_name="article.ris")
 
                 citation = format_citation(scholar_article)
                 st.write("**Formatted Citation:**")
